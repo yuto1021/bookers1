e974b48c6c0d0ffda0ca68a849f65339c4014fbe
yuto1021 <yuto10210720@gmail.com>
2021-07-24 12:27:04 +0000

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root 'homes#top'
+  get 'top' => 'homes#top'
+  get 'books/new' => 'books#new'
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+
+end
